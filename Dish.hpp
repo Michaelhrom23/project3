a4c789b2efe0aa979b8359c71cfb8021b8228dc7
Michaelhrom23 <44450089+Michaelhrom23@users.noreply.github.com>
2024-10-15 18:10:05 -0400

    Dish.hpp

--- Dish.hpp
+++ Dish.hpp
@@ -1,5 +1,3 @@
-
-
 #ifndef DISH_HPP
 #define DISH_HPP
 
@@ -8,114 +6,54 @@
 
 class Dish {
 public:
-    // CuisineType enum definition
+    
     enum CuisineType { ITALIAN, MEXICAN, CHINESE, INDIAN, AMERICAN, FRENCH, OTHER };
 
-    // Constructors
-    /**
-     * Default constructor.
-     * Initializes all private members with default values:
-     * - name: "UNKNOWN"
-     * - ingredients: Empty list
-     * - prep_time: 0
-     * - price: 0.0
-     * - cuisine_type: OTHER
-     */
+    
     Dish();
 
-    /**
-     * Parameterized constructor.
-     * @param name A reference to the name of the dish.
-     * @param ingredients A reference to a list of ingredients (default is an empty list).
-     * @param prep_time The preparation time in minutes (default is 0).
-     * @param price The price of the dish (default is 0.0).
-     * @param cuisine_type The cuisine type of the dish (a CuisineType enum) with default value OTHER.
-     * @post The private members are set to the values of the corresponding parameters.
-     */
-    Dish(const std::string& name, const std::vector<std::string>& ingredients = {}, int prep_time = 0, double price = 0.0, CuisineType cuisine_type = CuisineType::OTHER);
-
-    // Accessors
-    /**
-     * @return The name of the dish.
-     */
+   
+    Dish(const std::string& name, const std::vector<std::string>& ingredients, int prep_time, double price, CuisineType cuisine_type);
+    
+    
     std::string getName() const;
 
-    /**
-     * @return The list of ingredients used in the dish.
-     */
+   
     std::vector<std::string> getIngredients() const;
-
+    
     int getIngredientCount()const;
-
-    /**
-     * @return The preparation time in minutes.
-     */
+    
     int getPrepTime() const;
 
-    /**
-     * @return The price of the dish.
-     */
+    
     double getPrice() const;
 
-    /**
-     * @return The cuisine type of the dish in string form.
-     */
+    
     std::string getCuisineType() const;
 
-    // Mutators
-    /**
-     * Sets the name of the dish.
-     * @param name A reference to the new name of the dish.
-     * @post Sets the private member `name_` to the value of the parameter. If the name contains non-alphabetic characters, it is set to "UNKNOWN".
-     */
+    
     void setName(const std::string& name);
 
-    /**
-     * Sets the list of ingredients.
-     * @param ingredients A reference to the new list of ingredients.
-     * @post Sets the private member `ingredients_` to the value of the parameter.
-     */
+    
     void setIngredients(const std::vector<std::string>& ingredients);
 
-    /**
-     * Sets the preparation time.
-     * @param prep_time The new preparation time in minutes.
-     * @post Sets the private member `prep_time_` to the value of the parameter.
-     */
+    
     void setPrepTime(const int& prep_time);
 
-    /**
-     * Sets the price of the dish.
-     * @param price The new price of the dish.
-     * @post Sets the private member `price_` to the value of the parameter.
-     */
+    
     void setPrice(const double& price);
 
-    /**
-     * Sets the cuisine type of the dish.
-     * @param cuisine_type The new cuisine type of the dish (a CuisineType enum).
-     * @post Sets the private member `cuisine_type_` to the value of the parameter.
-     */
+    
     void setCuisineType(const CuisineType& cuisine_type);
 
-    // Display function
-    /**
-     * Displays the details of the dish.
-     * @post Outputs the dish's details, including name, ingredients, preparation time, price, and cuisine type, to the standard output.
-     * The information must be displayed in the following format:
-     *
-     * Dish Name: [Name of the dish]
-     * Ingredients: [Comma-separated list of ingredients]
-     * Preparation Time: [Preparation time] minutes
-     * Price: $[Price, formatted to two decimal places]
-     * Cuisine Type: [Cuisine type]
-     */
+    
     void display() const;
 
     
     bool operator==(const Dish& dish)const;
     
     bool operator!=(const Dish& dish)const;
+    
 
 private:
     std::string name_;
@@ -124,12 +62,7 @@ private:
     double price_;
     CuisineType cuisine_type_;
 
-    // Helper function to check if the name is valid
-    /**
-     * Checks if the name is valid.
-     * @param name The name to be validated.
-     * @return True if the name contains only alphabetic characters and spaces; false otherwise.
-     */
+    
     bool isValidName(const std::string& name) const;
 };
 
