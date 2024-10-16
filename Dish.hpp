6070b209c96e4e47e15da12ebc7311700a7f82e4
Michaelhrom23 <44450089+Michaelhrom23@users.noreply.github.com>
2024-10-15 21:05:44 -0400

    Dish.hpp

--- Dish.hpp
+++ Dish.hpp
@@ -5,49 +5,48 @@
 #include <vector>
 
 class Dish {
-public:
-    
+public: //public class
+    //cusintetype enum types
     enum CuisineType { ITALIAN, MEXICAN, CHINESE, INDIAN, AMERICAN, FRENCH, OTHER };
 
     
-    Dish();
+    Dish(); //default constructor
 
-   
+   //parameterized constructor, with a name, the ingredients, prep time, price and cuisine type
     Dish(const std::string& name, const std::vector<std::string>& ingredients, int prep_time, double price, CuisineType cuisine_type);
     
     
-    std::string getName() const;
-
+    std::string getName() const; //returns string type that is the name
    
-    std::vector<std::string> getIngredients() const;
+    std::vector<std::string> getIngredients() const; //returns a vector that is made of strings, gets ingredients
     
-    int getIngredientCount()const;
+    int getIngredientCount()const; //returns integer of the number of ingredients 
     
-    int getPrepTime() const;
+    int getPrepTime() const; //returns the time that it takes for the preparation in mins
 
     
-    double getPrice() const;
+    double getPrice() const; //returns double type
 
     
-    std::string getCuisineType() const;
+    std::string getCuisineType() const; //returns a string type that is a cuisine type
 
     
-    void setName(const std::string& name);
+    void setName(const std::string& name); //void method that sets the name to the parameter
 
     
-    void setIngredients(const std::vector<std::string>& ingredients);
+    void setIngredients(const std::vector<std::string>& ingredients); //void method that sets the ingredients to the parameter
 
     
-    void setPrepTime(const int& prep_time);
+    void setPrepTime(const int& prep_time); //void method that sets the preparation time of the function 
 
     
-    void setPrice(const double& price);
+    void setPrice(const double& price); //void method that sets the price 
 
     
-    void setCuisineType(const CuisineType& cuisine_type);
+    void setCuisineType(const CuisineType& cuisine_type); //void method that sets the cuisine type
 
     
-    void display() const;
+    void display() const; //void method that displays the data
 
     
     bool operator==(const Dish& dish)const;
@@ -55,15 +54,15 @@ public:
     bool operator!=(const Dish& dish)const;
     
 
-private:
+private: //private data members
     std::string name_;
-    std::vector<std::string> ingredients_;
+    std::vector<std :: string> ingredients_;
     int prep_time_;
     double price_;
     CuisineType cuisine_type_;
 
     
-    bool isValidName(const std::string& name) const;
+    bool isValidName(const std::string& name) const; //private meehtod
 };
 
-#endif // DISH_HPP
+#endif 
