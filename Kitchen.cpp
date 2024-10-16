04d6f52cb188cc81155fdbd1fe01a51cb1e6b297
Michaelhrom23 <44450089+Michaelhrom23@users.noreply.github.com>
2024-10-15 21:13:56 -0400

    Kitchen.cpp

--- Kitchen.cpp
+++ Kitchen.cpp
@@ -1,8 +1,8 @@
 #include "Kitchen.hpp"
 
-Kitchen :: Kitchen():totalPrep_time(0),countElaborate(0) {}
+Kitchen :: Kitchen():totalPrep_time(0),countElaborate(0) {} //default constructor
 
-bool Kitchen :: newOrder(const Dish &dish) {
+bool Kitchen :: newOrder(const Dish &dish) { //adds a new dish to the kitchen
     if (contains(dish)) {
         return false;
     }
@@ -18,7 +18,7 @@ bool Kitchen :: newOrder(const Dish &dish) {
     return true;
 }
 
-bool Kitchen :: serveDish(const Dish &dish) {
+bool Kitchen :: serveDish(const Dish &dish) { //serves dish/ removes the dish from the kitchen 
     if (contains (dish)) {
         totalPrep_time = totalPrep_time - dish.getPrepTime();
         if (dish.getIngredientCount()>= 5 &&dish.getPrepTime() >=60) {
@@ -33,11 +33,11 @@ bool Kitchen :: serveDish(const Dish &dish) {
     return false; 
 }
 
-int Kitchen :: getPrepTimeSum() const {
+int Kitchen :: getPrepTimeSum() const { //returns int type of the the summation of the preparation time
     return totalPrep_time;
 }
 
-int Kitchen :: calculateAvgPrepTime() const {
+int Kitchen :: calculateAvgPrepTime() const { //gets the average prep time 
     if (getCurrentSize()==0){
         return 0;
     }  
@@ -48,20 +48,20 @@ int Kitchen :: calculateAvgPrepTime() const {
 
 
 
-int Kitchen :: elaborateDishCount() {
+int Kitchen :: elaborateDishCount() { // returns in type that represents the count of elaborate dishes
 
     return countElaborate;
 }
 
-double Kitchen :: calculateElaboratePercentage() const {
-    if (getCurrentSize() == 0){
+double Kitchen :: calculateElaboratePercentage() const { //gets the percentage of elaborate foods
+    if (getCurrentSize() == 0){ //base case to avoid
         return 0.0;
     } 
     return std :: round(static_cast <double> (countElaborate)/getCurrentSize()*10000)/ 100;
 }
 
 
-int Kitchen :: tallyCuisineTypes(const std :: string &cuisineType) const {
+int Kitchen :: tallyCuisineTypes(const std :: string &cuisineType) const { //keeps a count of cuisine types
 
     int count{0};
 
@@ -74,7 +74,7 @@ int Kitchen :: tallyCuisineTypes(const std :: string &cuisineType) const {
     return count;
 }
 
-int Kitchen :: releaseDishesBelowPrepTime(const int &prepTime){
+int Kitchen :: releaseDishesBelowPrepTime(const int &prepTime){ //shows prep time of release dishes
 
     int removedCount{0};
 
@@ -82,7 +82,7 @@ int Kitchen :: releaseDishesBelowPrepTime(const int &prepTime){
 
         if (items_[k].getPrepTime() < prepTime) {
             totalPrep_time = totalPrep_time- items_[k].getPrepTime();
-            if (items_[k].getIngredientCount() >= 5 && items_[k].getPrepTime() >= 60) {
+            if (items_[k].getPrepTime() >= 60 && items_[k].getIngredientCount() >= 5) {
                 countElaborate-=1;
             }
 
@@ -95,11 +95,11 @@ int Kitchen :: releaseDishesBelowPrepTime(const int &prepTime){
     return removedCount;
 }
 
-int Kitchen :: releaseDishesOfCuisineType(const std :: string &cuisineType) {
+int Kitchen :: releaseDishesOfCuisineType(const std :: string &cuisineType) { //shows dishes of cuisine types
 
     int removedCount{0};
 
-    for (int j = 0; j < getCurrentSize();) {
+    for (int j = 0; j < getCurrentSize();) { //o(n) linear time complexity 
         if (items_[j].getCuisineType()==cuisineType) {
             totalPrep_time -= items_[j].getPrepTime();
             if (items_[j].getIngredientCount() >=5 && items_[j].getPrepTime()>= 60) {
@@ -113,6 +113,7 @@ int Kitchen :: releaseDishesOfCuisineType(const std :: string &cuisineType) {
     }
     return removedCount;
 }
+//displays the kitchen report
 void Kitchen :: kitchenReport() const {
 
     std :: cout << "ITALIAN: " << tallyCuisineTypes("ITALIAN") << "\n"
@@ -126,4 +127,3 @@ void Kitchen :: kitchenReport() const {
               << "ELABORATE: " << std::fixed << std::setprecision(2) 
               << calculateElaboratePercentage() << "%\n";
 }
-
